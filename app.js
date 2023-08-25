e39c16552a0c453b6f289c6db169c9fbff0f58ce
Nikhil kumar <89766775+Nikhilnetha119@users.noreply.github.com>
2023-08-25 22:44:34 +0530

    edit

--- app.js
+++ app.js
@@ -1,22 +1,55 @@
-const userForm = document.getElementById("userForm");
-const submitButton = document.getElementById("submitButton");
-        // Add an event listener to the submit button
-submitButton.addEventListener("click", function() {
-    // Get the values entered by the user
+// const submitButton = document.getElementById("submitButton");
+// const userForm = document.getElementById("userForm");
+
+submitButton.addEventListener('click', function() {
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
 
-    // Create an object to store the user details
     const userDetails = {
         name: name,
         email: email
     };
 
-    // Convert the object to a JSON string
     const userDetailsJSON = JSON.stringify(userDetails);
 
-    // Store the JSON string in local storage
     localStorage.setItem("userDetails", userDetailsJSON);
+    Show(userDetails)
 
     alert("User details have been stored in local storage.");
 });
+
+function Show(userDetails){
+    const parentChild  = document.getElementById('userList');
+    const child = document.createElement('li');
+    child.textContent = userDetails.name + ' - ' + userDetails.email;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', function() {
+        parentChild.removeChild(child); // Remove from UI
+        const storedUserDetails = JSON.parse(localStorage.getItem('userDetails'));
+        if (storedUserDetails && storedUserDetails.name === userDetails.name && storedUserDetails.email === userDetails.email) {
+            localStorage.removeItem('userDetails');
+        }
+    });
+
+    const editButton = document.createElement('button');
+    editButton.textContent = 'Edit';
+    editButton.addEventListener('click', function() {
+        const newName = prompt("Enter new name:", userDetails.name);
+        const newEmail = prompt("Enter new email:", userDetails.email);
+            if (newName && newEmail) {
+                userDetails.name = newName;
+                userDetails.email = newEmail;
+                localStorage.setItem('userDetails', JSON.stringify(storedUserDetails));
+                listItem.textContent = userDetails.name + ' - ' + userDetails.email;
+            }
+        //document.getElementById('name').value = userDetails.name;
+        //document.getElementById('email').value = userDetails.email;
+    });
+    
+    child.appendChild(editButton);
+    child.appendChild(deleteButton);
+    parentChild.appendChild(child);
+
+}
